8ce25f314c985664bfd1402ba2b97bd4867cc620
Wilhelm Matilainen <wilhelm.matilainen@gmail.com>
2015-08-17 00:42:07 +0300

    index.js updated

--- index.js
+++ index.js
@@ -14,14 +14,32 @@ var _buildDebugNode = require('./build/debug.node');
 
 var _buildDebugNode2 = _interopRequireDefault(_buildDebugNode);
 
+var _debug = require('debug');
+
+var _debug2 = _interopRequireDefault(_debug);
+
+var log = (0, _debug2['default'])('natrium');
+
 var Natrium = (function () {
 	function Natrium() {
 		_classCallCheck(this, Natrium);
+
+		this.size = {
+			'public': _buildDebugNode2['default'].size_sign_public,
+			secret: _buildDebugNode2['default'].size_sign_secret,
+			seed: _buildDebugNode2['default'].size_seed,
+			signature: _buildDebugNode2['default'].size_sign,
+			box_public: _buildDebugNode2['default'].size_box_public,
+			box_secret: _buildDebugNode2['default'].size_box_secret,
+			box_key: _buildDebugNode2['default'].size_box_keyt
+		};
 	}
 
 	_createClass(Natrium, [{
 		key: 'random',
 		value: function random(size) {
+			if (!Number.isInteger(size) || size < 1) return Promise.reject(new Error('size should be an integer number and greater than 0'));
+
 			return new Promise(function (success) {
 				_buildDebugNode2['default'].random(size, success);
 			});
@@ -36,8 +54,12 @@ var Natrium = (function () {
 	}, {
 		key: 'sign_keypair',
 		value: function sign_keypair(seed) {
-			return new Promise(function (success) {
-				_buildDebugNode2['default'].sign_keypair(seed, function (pk, sk) {
+			if (!Buffer.isBuffer(seed) || seed.length != this.size.seed) return Promise.reject(new Error('seed should be a Buffer of size ' + this.size.seed));
+
+			return new Promise(function (success, fail) {
+				_buildDebugNode2['default'].sign_keypair(seed, function (error, pk, sk) {
+					if (error) return fail(error);
+
 					success({ 'public': pk, secret: sk, seed: seed });
 				});
 			});
@@ -45,19 +67,104 @@ var Natrium = (function () {
 	}, {
 		key: 'new_sign_keypair',
 		value: function new_sign_keypair() {
-			return this.random_seed().then(this.sign_keypair);
+			var _this = this;
+
+			return this.random_seed().then(function (seed) {
+				return _this.sign_keypair(seed);
+			});
+		}
+	}, {
+		key: 'sign',
+		value: function sign(secret, message) {
+			if (!Buffer.isBuffer(secret) || secret.length != this.size.secret) return Promise.reject(new Error('secret should be a Buffer of size ' + this.size.secret));
+
+			if (!Buffer.isBuffer(message) || message.length === 0) return Promise.reject(new Error('message should be a Buffer of a size greater than 0'));
+
+			return new Promise(function (success, fail) {
+				_buildDebugNode2['default'].sign(secret, message, function (error, signature) {
+					if (error) return fail(error);
+
+					success(signature);
+				});
+			});
+		}
+	}, {
+		key: 'verify',
+		value: function verify(pk, signature, message) {
+			if (!Buffer.isBuffer(pk) || pk.length != this.size['public']) return Promise.reject(new Error('public key should be a Buffer of size ' + this.size['public']));
+
+			if (!Buffer.isBuffer(signature) || signature.length === 0) return Promise.reject(new Error('signature should be a Buffer of a size greater than 0'));
+
+			if (!Buffer.isBuffer(message) || message.length === 0) return Promise.reject(new Error('message should be a Buffer of a size greater than 0'));
+
+			return new Promise(function (success, fail) {
+				_buildDebugNode2['default'].verify(pk, signature, message, function (error) {
+					if (error) return fail(error);
+
+					success();
+				});
+			});
+		}
+	}, {
+		key: 'box_keypair',
+		value: function box_keypair() {
+			return new Promise(function (success, fail) {
+				_buildDebugNode2['default'].box_keypair(function (error, pk, sk) {
+					if (error) return fail(error);
+
+					success({ 'public': pk, secret: sk });
+				});
+			});
+		}
+
+		// secret is own secret key
+		// pk is the someone else's public key
+	}, {
+		key: 'box_key',
+		value: function box_key(secret, pk) {
+			if (!Buffer.isBuffer(pk) || pk.length != this.size.box_public) return Promise.reject(new Error('public key should be a Buffer of size ' + this.size.box_public));
+
+			if (!Buffer.isBuffer(secret) || secret.length != this.size.box_secret) return Promise.reject(new Error('secret key should be a Buffer of size ' + this.size.box_secret));
+
+			return new Promise(function (success, fail) {
+				_buildDebugNode2['default'].box_key(pk, secret, function (error, key) {
+					if (error) return fail(error);
+
+					success(key);
+				});
+			});
 		}
 	}]);
 
 	return Natrium;
 })();
 
-exports['default'] = Natrium;
+exports.Natrium = Natrium;
 
-var n = new Natrium();
+var na = new Natrium();
+exports['default'] = na;
 
-n.new_sign_keypair().then(function (key) {
-	console.log(key);
-	return n.sign_keypair(key.seed).then(console.log);
-})['catch'](console.error);
-module.exports = exports['default'];
+//na.new_sign_keypair().then(function (key) {
+//	return na.random(4).then(function (message) {
+//		return na.sign(key.secret, message).then(function (signature) {
+//			log({message, signature});
+//			return na.verify(key.public, signature, message).then(() => log('Verified!'));
+//		});
+//	});
+//}).catch(log);
+
+na.box_keypair().then(function (alice) {
+	log({ alice: alice });
+
+	return na.box_keypair().then(function (bob) {
+		log({ bob: bob });
+
+		return na.box_key(alice.secret, bob['public']).then(function (key) {
+			log({ key: key });
+		}).then(function () {
+			return na.box_key(bob.secret, alice['public']).then(function (key) {
+				log({ key: key });
+			});
+		});
+	});
+})['catch'](log);
